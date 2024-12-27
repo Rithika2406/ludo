142dc8b092af0683b86af73382ae84418c193b40
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-27 20:54:26 +0530

    Add files via upload

---
+++ ludo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner S=new Scanner(System.in);
+		int t=S.nextInt();
+		while(t>0)
+		{
+		    int x=S.nextInt();
+		    if(x==6)
+		    {
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		    t--;
+		}
+
+	}
